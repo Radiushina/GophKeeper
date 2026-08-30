065e77f646513920a9ade4d86c2e3d902eba4f5f
r.oks <otl@fivegen.ru>
2026-08-30 18:12:16 +0300

    fix(migrate): password length is limited

--- internal/migrations/postgres/20260823105450_users.up.sql
+++ internal/migrations/postgres/20260823105450_users.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     login VARCHAR(200) NOT NULL UNIQUE,
-    password TEXT NOT NULL
+    password VARCHAR(60) NOT NULL
 );
